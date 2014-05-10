72aa42b10824203daf81bec106ec193993bdbf35
e7aso <luszjo36@gmail.com>
2014-05-10 15:23:30 +0200

    main

--- main.cpp
+++ main.cpp
@@ -18,7 +18,27 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Graf *g = new MGraf();
+	g->losujGraf(50, 1);
 	
+	int **temp = NULL, *temp1 = NULL, *temp2 = NULL;
+
+	
+	g->sptBellmanFord(10, temp, temp1);
+	
+	for(int i = 0; i < g->getNumOfVertices(); i++)
+		delete temp[i];
+	delete temp;
+	delete temp1;
+
+	temp = NULL;
+	temp1 = NULL;
+
+	g->sptDijkstra(10, temp1, temp2);
+	delete temp1;
+	delete temp2;
+
+	delete g;
 
 	cout << "\n\nWcisnij dowolny klawisz, aby zakonczyc program...";
 	_getch();
